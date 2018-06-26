05e6e85cf34a8306ea72c125f2fddb5a704aa5e5
Thiago Luís <thiagoluismo@gmail.com>
2018-06-26 16:31:39 -0300

    Posicionamento das urls.

--- application/views/questoes/exibir.php
+++ application/views/questoes/exibir.php
@@ -1,4 +1,33 @@
-<table class="table table-bordered table-striped">
+<?php
+
+/* URLs */
+$listar = site_url(['questoes', 'itens', $questao['ID_QUESTAO']]);
+$cadastrar = site_url(['questoes', 'cadastrarItem', $questao['ID_QUESTAO']]);
+$excluir = site_url('questoes/excluirItem');
+$alterar = site_url('questoes/alterarItem');
+$listarQuestao = site_url('questoes');
+$alterarQuestao = site_url(['questoes', 'formulario', $questao['ID_QUESTAO']]);
+?>
+
+<p>
+    <a href="<?= $alterarQuestao ?>" class="btn btn-primary">Alterar</a>
+    <a href="<?= $listarQuestao ?>" class="btn btn-link">Questões</a>
+</p>
+
+<div class="panel panel-default">
+    <div class="panel-body">
+        <h4>Questão</h4>
+        <div id="editor-questao"><?= $questao['DESCRICAO_QUESTAO'] ?></div>
+    </div>
+</div>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <h4>Comando</h4>
+        <div id="editor-comando"><?= $questao['COMANDO_QUESTAO'] ?></div>
+    </div>
+</div>
+
+<table class="table table-bordered table-striped hide">
    <tbody>
        <?php foreach($questao as $campo => $v): ?>
            <?php if (!in_array($campo, ['DESCRICAO_QUESTAO', 'COMANDO_QUESTAO'])) continue; ?>
@@ -10,45 +39,51 @@
    </tbody>
 </table>
 
-<?= form_open('questoes/cadastrarItem/' . $questao['ID_QUESTAO'], [
-    'id' => 'cadastrar-item'
-]) ?>
 
-    <input type="hidden" name="id_questao" value="<?= $questao['ID_QUESTAO'] ?>">
+<div class="panel panel-default">
+    <div class="panel panel-heading">Itens</div>
+    <div class="panel-body">
+        <?= form_open('questoes/cadastrarItem/' . $questao['ID_QUESTAO'], [
+            'id' => 'cadastrar-item'
+        ]) ?>
 
-    <div class="row">
-        <div class="form-group col-sm-2">
-            <label>Item</label>
-            <select name="letra_item" class="form-control">
-                <?php foreach(['A','B','C'] as $item): ?>
-                    <option value="<?= $item ?>"><?= $item ?></option>
-                <?php endforeach; ?>
-            </select>
-            <div></div>
-        </div>
+            <input type="hidden" name="id_questao" value="<?= $questao['ID_QUESTAO'] ?>">
 
-        <div class="form-group col-sm-8">
-            <label>Descrição</label>
-            <input name="descricao" class="form-control"></input>
-            <div></div>
-        </div>
+            <div class="row">
+                <div class="form-group col-sm-2">
+                    <label>Item</label>
+                    <select name="letra_item" class="form-control">
+                        <?php foreach(['' => 'Selecione', 'A','B','C','D','E'] as $item): ?>
+                            <option value="<?= $item ?>"><?= $item ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <div></div>
+                </div>
 
-        <label>&nbsp;</label>
-        <div class="form-group">
-            <button class="btn btn-primary">Cadastrar</button>
-        </div>
+                <div class="form-group col-sm-8">
+                    <label>Descrição</label>
+                    <input name="descricao" class="form-control"></input>
+                    <div></div>
+                </div>
+
+                <label>&nbsp;</label>
+                <div class="form-group">
+                    <button class="btn btn-primary">Cadastrar</button>
+                </div>
+            </div>
+        <?= form_close() ?>
+
+        <table id="itens" class="table table-bordered table-striped table-primary">
+            <thead>
+                <tr>
+                    <th>Letra</th>
+                    <th>Descrição</th>
+                    <th class="text-center">Ações</th>
+                </tr>
+            </thead>
+        </table>
     </div>
-<?= form_close() ?>
-
-<table id="itens" class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Letra</th>
-            <th>Descrição</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-</table>
+</div>
 
 <div id="modal-alterar-item" class="modal fade" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-lg" role="document">
@@ -62,24 +97,18 @@
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-<div class="panel panel-default">
-    <div class="panel-body">
-        <div id="editor"></div>
-    </div>
-</div>
-
 <?php
 $listar = site_url(['questoes', 'itens', $questao['ID_QUESTAO']]);
 $cadastrar = site_url(['questoes', 'cadastrarItem', $questao['ID_QUESTAO']]);
 $excluir = site_url('questoes/excluirItem');
 $alterar = site_url('questoes/alterarItem');
 $js = <<<JS
-var js = function() {
+init.push(function() {
     var formCadastrar = $('#cadastrar-item');
     var formAlterar = '#alterar-item';
     var mod = $('#modal-alterar-item');
-    var btnExcluir = $('<a class="excluir" href="$excluir">excluir</a>');
-    var btnAlterar = $('<a class="alterar" href="$alterar">alterar</a>');
+    var btnExcluir = $('<a class="excluir" href="$excluir" title="Excluir"><i class="fas fa-trash"></i></a>');
+    var btnAlterar = $('<a class="alterar" href="$alterar" title="Alterar"><i class="fas fa-pen-alt"></i></a>');
     var tb = $('#itens').DataTable({
         "dom": "t",
         "bSort": false,
@@ -95,21 +124,20 @@ var js = function() {
                     var alterar = btnAlterar.clone().attr('data-id', row.ID_ITEM);
                     var excluir = btnExcluir.clone().attr('data-id', row.ID_ITEM);
                     return alterar.prop('outerHTML') + ' ' + excluir.prop('outerHTML');
+                },
+                "createdCell":  function (td, cellData, rowData, row, col) {
+                   $(td).attr('class', 'text-center');
                 }
             },
         ]
     });
 
-    var quill = new Quill('#editor', {
-      modules: {
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          ['image', 'code-block']
-        ]
-      },
-      placeholder: 'Texto rico...',
-      theme: 'snow'  // or 'bubble'
+    var editorQuestao = new Quill('#editor-questao', {
+      readOnly: true
+    });
+
+    var editorComando = new Quill('#editor-comando', {
+      readOnly: true
     });
 
     function atualizar() {
@@ -121,7 +149,7 @@ var js = function() {
         $.post('$cadastrar', formCadastrar.serialize(), function(data) {
             if (data.status) {
                 atualizar();
-                form.get(0).reset();
+                formCadastrar.get(0).reset();
             }
         },'json');
     }
@@ -157,9 +185,7 @@ var js = function() {
     $(document).on('submit', formAlterar, alterar);
     $(document).on('click', '.alterar', getItem);
     $(document).on('click', '.excluir', excluir);
-}
-
-init.push(js);
+});
 JS;
 ?>
 

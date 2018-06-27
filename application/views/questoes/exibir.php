7415bd46394f9ad0b0f369bccb267cfff76ffa90
Thiago Luís <thiagoluismo@gmail.com>
2018-06-27 00:47:20 -0300

    Configuração para inserir imagem.

--- application/views/questoes/exibir.php
+++ application/views/questoes/exibir.php
@@ -39,40 +39,10 @@ $alterarQuestao = site_url(['questoes', 'formulario', $questao['ID_QUESTAO']]);
    </tbody>
 </table>
 
-
-<div class="panel panel-default">
+<div id="panel-itens" class="panel panel-default">
     <div class="panel panel-heading">Itens</div>
     <div class="panel-body">
-        <?= form_open('questoes/cadastrarItem/' . $questao['ID_QUESTAO'], [
-            'id' => 'cadastrar-item'
-        ]) ?>
-
-            <input type="hidden" name="id_questao" value="<?= $questao['ID_QUESTAO'] ?>">
-
-            <div class="row">
-                <div class="form-group col-sm-2">
-                    <label>Item</label>
-                    <select name="letra_item" class="form-control">
-                        <?php foreach(['' => 'Selecione', 'A','B','C','D','E'] as $item): ?>
-                            <option value="<?= $item ?>"><?= $item ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                    <div></div>
-                </div>
-
-                <div class="form-group col-sm-8">
-                    <label>Descrição</label>
-                    <input name="descricao" class="form-control"></input>
-                    <div></div>
-                </div>
-
-                <label>&nbsp;</label>
-                <div class="form-group">
-                    <button class="btn btn-primary">Cadastrar</button>
-                </div>
-            </div>
-        <?= form_close() ?>
-
+        <div class="form"></div>
         <table id="itens" class="table table-bordered table-striped table-primary">
             <thead>
                 <tr>
@@ -102,16 +72,43 @@ $listar = site_url(['questoes', 'itens', $questao['ID_QUESTAO']]);
 $cadastrar = site_url(['questoes', 'cadastrarItem', $questao['ID_QUESTAO']]);
 $excluir = site_url('questoes/excluirItem');
 $alterar = site_url('questoes/alterarItem');
+$upload = site_url('questoes/upload');
 $js = <<<JS
 init.push(function() {
-    var formCadastrar = $('#cadastrar-item');
+    var panelItens = $('#panel-itens');
+    var descricao = $('#descricao');
+    var formCadastrar = '#cadastrar-item';
     var formAlterar = '#alterar-item';
     var mod = $('#modal-alterar-item');
     var btnExcluir = $('<a class="excluir" href="$excluir" title="Excluir"><i class="fas fa-trash"></i></a>');
     var btnAlterar = $('<a class="alterar" href="$alterar" title="Alterar"><i class="fas fa-pen-alt"></i></a>');
+    var editorCfg = {
+        modules: {
+            formula: true,
+            toolbar: [
+                ['bold', 'italic', 'underline', 'strike'],
+
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                [{ 'script': 'sub'}, { 'script': 'super' }],
+                [{ 'indent': '-1'}, { 'indent': '+1' }],
+
+                [{ 'color': [] }, { 'background': [] }],
+                [{ 'align': [] }],
+
+                ['formula','image'],
+
+                ['clean']
+            ],
+        },
+        theme: 'snow'
+    };
     var tb = $('#itens').DataTable({
         "dom": "t",
         "bSort": false,
+        "iDisplayLength": Infinity,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum item cadastrado."
+        },
         "ajax": {
             "url": "$listar",
             "dataSrc": ""
@@ -132,26 +129,35 @@ init.push(function() {
         ]
     });
 
-    var editorQuestao = new Quill('#editor-questao', {
-      readOnly: true
-    });
-
-    var editorComando = new Quill('#editor-comando', {
-      readOnly: true
-    });
+    new Quill('#editor-questao', {readOnly: true});
+    new Quill('#editor-comando', {readOnly: true});
 
     function atualizar() {
         tb.ajax.reload();
     }
 
+    function initFormCadastrar() {
+        var descricao = $(formCadastrar).find('#descricao');
+        var editorDescricao = new Quill(formCadastrar + ' #editor-descricao', editorCfg);
+
+        editorDescricao.getModule('toolbar').addHandler('image', selectLocalImage);
+        editorDescricao.on('text-change', function() {
+            var html = editorDescricao.root.innerHTML;
+            descricao.val(html);
+        });
+    };
+
     function cadastrar(e) {
         e.preventDefault();
-        $.post('$cadastrar', formCadastrar.serialize(), function(data) {
-            if (data.status) {
+        var form = $(formCadastrar);
+        $.post('$cadastrar', form.serialize(), function(data) {
+            panelItens.find('.form').html(data);
+            form = $(formCadastrar);
+            if (form.data('save')) {
                 atualizar();
-                formCadastrar.get(0).reset();
             }
-        },'json');
+            initFormCadastrar();
+        });
     }
 
     function alterar(e) {
@@ -168,6 +174,15 @@ init.push(function() {
         var url = this.href + '/' + $(this).data('id');
         $.get(url, function(data) {
             mod.find('.modal-body').html(data);
+            var descricao = $(formAlterar).find('#descricao');
+            var editorDescricao = new Quill(formAlterar + ' #editor-descricao', editorCfg);
+
+            editorDescricao.getModule('toolbar').addHandler('image', selectLocalImage);
+            editorDescricao.on('text-change', function() {
+                var html = editorDescricao.root.innerHTML;
+                descricao.val(html);
+            });
+
             mod.modal('show');
         });
     }
@@ -181,10 +196,52 @@ init.push(function() {
         }
     }
 
-    formCadastrar.on('submit', cadastrar);
+    // https://github.com/quilljs/quill/issues/1089
+    function selectLocalImage() {
+        var quill = this.quill;
+        var input = $('<input type="file">');
+        input.click();
+
+        input.change(function() {
+            var file = input[0].files[0];
+
+            if (/^image\//.test(file.type)) {
+                saveToServer(file, quill);
+            } else {
+                alert('Apenas imagem é válido.');
+            }
+        });
+    }
+
+    function saveToServer(file, quill) {
+        var fd = new FormData();
+        var xhr = new XMLHttpRequest();
+
+        fd.append('image', file);
+        xhr.open('POST', '$upload');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var url = JSON.parse(xhr.responseText).url;
+                insertToEditor(url, quill);
+            }
+        };
+        xhr.send(fd);
+    }
+
+    function insertToEditor(url, quill) {
+        var range = quill.getSelection();
+        quill.insertEmbed(range.index, 'image', url);
+    }
+
+    $(document).on('submit', formCadastrar, cadastrar);
     $(document).on('submit', formAlterar, alterar);
     $(document).on('click', '.alterar', getItem);
     $(document).on('click', '.excluir', excluir);
+
+    $.get('$cadastrar', function(data) {
+        panelItens.find('.form').html(data);
+        initFormCadastrar();
+    });
 });
 JS;
 ?>
